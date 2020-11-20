6dd67c8214285d2151fe40464b069715cd693b7d
GingerBoy14 <maks.27.04.2002@gmail.com>
2020-11-20 19:02:39 +0200

    fix project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './style.scss'
+
+const App = () => {
+  return (
+    <div className="row h-100 justify-content-center">
+      <div className="col-4">
+        <h2>Todo list</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
