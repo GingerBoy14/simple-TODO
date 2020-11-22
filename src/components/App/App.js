18db15a96934e3c5a3d3017d3c592552a1ee6b94
Aleksandr <aleksandr.golyk@gmail.com>
2020-11-22 12:44:04 +0200

    init dropdown branch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './style.scss'
+import Dropdown from '../DropDown/Dropdown'
 
 const App = () => {
   return (
     <div className="row h-100 justify-content-center">
       <div className="col-4">
         <h2>Todo list</h2>
+        <Dropdown />
       </div>
     </div>
   )
