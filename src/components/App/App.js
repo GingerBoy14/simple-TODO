289e694fb2e902781d32d44d3dc160f4c5d9d05b
olenashevchuk <olenashevchuk0@gmail.com>
2020-11-23 18:15:10 +0200

    added new class app-wrapper

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,8 @@ import './style.scss'
 
 const App = () => {
   return (
-    <div className="row h-100 justify-content-center">
-      <div className="col-4">
-        <h2>Todo list</h2>
-      </div>
+    <div className="container app-wrapper ">
+      <div className="row h-100 justify-content-center">Hello, world!</div>
     </div>
   )
 }
