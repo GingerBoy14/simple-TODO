e4e06de53fc56684263186d83ba73a0a0a0047c5
olenashevchuk <olenashevchuk0@gmail.com>
2020-12-03 12:17:05 +0200

    edited jsconfig.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import 'antd/dist/antd.css'
+
 const App = () => {
   return <h2>HElloo</h2>
 }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from 'react'
+import { useReducer } from 'react'
 
 import storeContext from './context'
 import rootReducer from '../reducer/index'
