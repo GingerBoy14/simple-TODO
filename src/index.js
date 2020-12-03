636c1cf46ec335f44cc5b36009696389fd169200
GingerBoy14 <maks.27.04.2002@gmail.com>
2020-12-03 12:36:35 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components'
-import { Provider } from 'Context'
-import 'bootstrap'
+import { Provider } from 'context'
 import 'antd/dist/antd.css'
 
 ReactDOM.render(
