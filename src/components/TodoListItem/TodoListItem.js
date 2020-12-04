a67a95233f6d5f419603dd5359d5528165273d47
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2020-12-04 13:56:38 +0200

    addition for previous commit

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,10 +1,9 @@
-import { List, Typography } from 'antd'
+import { List, Typography, Checkbox, Space } from 'antd'
 import { useStoreContext } from '../../context'
 import { LeftOutlined } from '@ant-design/icons'
 
 const { Item } = List
 const { Text } = Typography
-const data = ['First task.', 'Second task.', 'Third task.', 'Fourth task.']
 
 const TodoListItem = (props) => {
   const { id, text, status } = props
@@ -22,12 +21,12 @@ const TodoListItem = (props) => {
         justifyContent: 'space-between',
         marginBottom: '5px'
       }}>
-      <Text
-        delete={status.done}
-        mark={status.important}
-        onClick={() => dispatch({ type: 'SET_DONE', payload: id })}>
-        {text}
-      </Text>
+      <Space>
+        <Checkbox checked={status.done} />
+        <Text onClick={() => dispatch({ type: 'SET_DONE', payload: id })}>
+          {text}
+        </Text>
+      </Space>
       <LeftOutlined onClick={() => console.log('Clicked')} />
     </Item>
   )
