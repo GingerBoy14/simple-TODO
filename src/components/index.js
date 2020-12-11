ccad97a0d89b802350646d953d542ca7f25ff2a0
Aleksandr <aleksandr.golyk@gmail.com>
2020-12-11 11:29:16 +0200

    added losed Search

--- src/components/index.js
+++ src/components/index.js
@@ -3,17 +3,23 @@ import { AddTodo } from './AddTodo'
 import { Header } from './Header'
 import 'antd/dist/antd.css'
 import { TodoList } from './TodoList'
+import { Search } from './Search'
 
 const App = () => {
   return (
     <Row justify="center">
-      <Col xs={22} sm={22} md={14} lg={10} xl={9} xxl={8} flex={1}>
-        <Row>
-          <Col flex={1}>
+      <Col xs={22} sm={22} md={14} lg={10} xl={9} xxl={8}>
+        <Row gutter={[0, 8]}>
+          <Col style={{ width: '100%' }}>
             <Header />
           </Col>
         </Row>
-        <Row gutter={[0, 8]} justify="center">
+        <Row gutter={[0, 8]}>
+          <Col flex="auto">
+            <Search />
+          </Col>
+        </Row>
+        <Row gutter={[0, 8]}>
           <Col flex={1}>
             <TodoList />
           </Col>
