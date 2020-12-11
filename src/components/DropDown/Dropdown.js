5d97f4e7fbf31d30a7835b1a44f48602c983e24f
Aleksandr <aleksandr.golyk@gmail.com>
2020-12-11 12:06:04 +0200

    added DropDownh

--- src/components/DropDown/Dropdown.js
+++ src/components/DropDown/Dropdown.js
@@ -11,6 +11,7 @@ import { useStoreContext } from 'context'
 const DropDown = (props) => {
   const { id, status } = props
   const { dispatch } = useStoreContext()
+
   const DropDownItem = (
     <Menu>
       <Menu.Item
@@ -39,9 +40,9 @@ const DropDown = (props) => {
   )
 
   return (
-    <Dropdown overlay={DropDownItem} placement="bottomRight" trigger="click">
+    <DropDown overlay={DropDownItem} placement="bottomRight" trigger="click">
       <DownOutlined />
-    </Dropdown>
+    </DropDown>
   )
 }
 export default DropDown
