44cfbb1ad402707abec1f4a4542b155a5c75b103
Aleksandr <aleksandr.golyk@gmail.com>
2020-12-22 12:14:44 +0200

    changed dropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,11 +15,11 @@ function handleMenuClick(e) {
 const menu = (
   <Menu onClick={handleMenuClick}>
     <Menu.Item key="1">
-      <Link to="/">Dashboard</Link>
+      <Link to="/">Profile</Link>
     </Menu.Item>
-    <Menu.Item key="2">
-      <Link to="/change-password">Change password</Link>
-    </Menu.Item>
+    {/*<Menu.Item key="2">*/}
+    {/*  <Link to="/change-password">Change password</Link>*/}
+    {/*</Menu.Item>*/}
     <Menu.Item key="3">3rd item</Menu.Item>
   </Menu>
 )
